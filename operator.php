ef7a7fe5be4acb9e6f671c292a581f4565a91e0b
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-22 15:51:14 +0900

    비교연산자

--- operator.php
+++ operator.php
@@ -19,14 +19,30 @@
 
     // echo $num1. " ". $num2
 
-    $tngNum = 10;
-    // echo $tngNum + $tngNum  ."\n". $tngNum / 5 ."\n". $tngNum - 4 ."\n". $tngNum % 7 ."\n". $tngNum * 3;
-    // echo "\n". $tngNum + 10 / 5 - 4 % 7 * 3;
-    $tngNum += $tngNum;
-    $tngNum /= 5;
-    $tngNum -= 4;
-    $tngNum %= 7;
-    $tngNum *= 3;
+    // $tngNum = 10;
+    // // echo $tngNum + $tngNum  ."\n". $tngNum / 5 ."\n". $tngNum - 4 ."\n". $tngNum % 7 ."\n". $tngNum * 3;
+    // // echo "\n". $tngNum + 10 / 5 - 4 % 7 * 3;
+    // $tngNum += $tngNum;
+    // $tngNum /= 5;
+    // $tngNum -= 4;
+    // $tngNum %= 7;
+    // $tngNum *= 3;
 
-    echo $tngNum;
+    // echo $tngNum;
+
+    // var_dump(1 > 2);
+    // var_dump(1 >= 2);
+    // var_dump(1 == 1);
+    // var_dump(1 === '1');
+    // var_dump(1 !== 2);
+    // var_dump(1 == 1 && 2 == 3);
+    // var_dump(1 == 1 || 2 == 3);
+
+
+    $value = 999991;
+    $value++;
+    // print $value . "\n";
+    $value = $value * 10;
+    print $value . "\n"
+
 ?>
