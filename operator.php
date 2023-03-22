bf1f4ae7b2371f06946e810c2cc98ced0ff11547
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-22 12:50:51 +0900

    후위,전위,,축약

--- operator.php
+++ operator.php
@@ -1,8 +1,21 @@
 <?php
 
-echo 1 + 1;
-echo "\n". 1 - 1;
-echo "\n". 2 * 3;
-echo "\n". 10 / 2;
-echo "\n". 9 % 7;
+    // echo 1 + 1;
+    // echo "\n". 1 - 1;
+    // echo "\n". 2 * 3;
+    // echo "\n". 10 / 2;
+    // echo "\n". 9 % 7;
+
+    // echo "\n". 10 - 5 * 8;
+
+    $num1 = 1;
+    $num2 = 1;
+
+    // echo "\n". $num1. ++$num1. $num1. $num2;
+    // echo "\n". $num1. $num1++. $num1. $num2;
+
+    $num1 = $num1 +1;
+    $num2 += 1;
+
+    echo $num1. " ". $num2
 ?>
