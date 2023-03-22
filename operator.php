de81bb90ea34ddfea06ccaa738db1e19ef4822fa
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-22 14:27:09 +0900

    대입연산자 예제

--- operator.php
+++ operator.php
@@ -8,14 +8,25 @@
 
     // echo "\n". 10 - 5 * 8;
 
-    $num1 = 1;
-    $num2 = 1;
+    // $num1 = 1;
+    // $num2 = 1;
 
     // echo "\n". $num1. ++$num1. $num1. $num2;
     // echo "\n". $num1. $num1++. $num1. $num2;
 
-    $num1 = $num1 +1;
-    $num2 += 1;
+    // $num1 = $num1 +1;
+    // $num2 += 1;
 
-    echo $num1. " ". $num2
+    // echo $num1. " ". $num2
+
+    $tngNum = 10;
+    // echo $tngNum + $tngNum  ."\n". $tngNum / 5 ."\n". $tngNum - 4 ."\n". $tngNum % 7 ."\n". $tngNum * 3;
+    // echo "\n". $tngNum + 10 / 5 - 4 % 7 * 3;
+    $tngNum += $tngNum;
+    $tngNum /= 5;
+    $tngNum -= 4;
+    $tngNum %= 7;
+    $tngNum *= 3;
+
+    echo $tngNum;
 ?>
