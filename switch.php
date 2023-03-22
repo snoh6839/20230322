f696420993bd1734b79ea6004f28e06cacb060fe
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-22 18:02:17 +0900

    switch

---
+++ switch.php
@@ -0,0 +1,12 @@
+<?php
+    $score = 80;
+    switch ($score) {
+        case 100:
+            echo "100점입니다";
+            break;
+        
+        default:
+            echo "아...";
+            break;
+    }
+?>
